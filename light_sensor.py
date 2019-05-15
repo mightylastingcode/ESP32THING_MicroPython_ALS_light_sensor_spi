9acdd209da06e80f4dee18aff823cf303595adcc
Michael Li <mikelisfbay@yahoo.com>
2019-05-15 10:04:25 -0700

    test and fix code for port 2 and port 3 options.

--- light_sensor.py
+++ light_sensor.py
@@ -1,8 +1,11 @@
 '''
-	Project: SPI Light Sensor using hardwareware spi driver (id=2).
+	Project: SPI Light Sensor 
 
-	version 3: change software version to hardware version
-	           use 80Mhz instead of 200k hz
+	SPI Port:
+	 Port 1: fixed hardware spi port location (id=1).
+	 Port 2: fixed hardware spi port location (id=2).
+	 None: Software spi bit bang driver (any pin defined by users)
+
 '''
 
 import machine
@@ -17,6 +20,7 @@ class LIGHT_Sensor:
 	Represents a light sensor
 	'''
 	def __init__(self,freq,port,csbpin,sckpin=18,mosipin=23,misopin=19):
+		print ("CSB pin = %d" % csbpin)
 		self.csb  = machine.Pin(csbpin, machine.Pin.OUT)
 		self.csb.value(1)   # disable
 
@@ -25,10 +29,13 @@ class LIGHT_Sensor:
 		# phase=0 means sample on the first edge of SCK, phase=1 means the second
 		# 1M, 10M, 20M ok (1M is more stable)
 		if (port == 1):
+			print("Use hardware spi port 1: sck 14, mosi 13, miso 12")
 			self.spi = SPI(1, baudrate=freq, polarity=1, phase=1, bits=8, firstbit=0, sck=Pin(14), mosi=Pin(13), miso=Pin(12))
 		elif (port == 2):
-			self,spi = SPI(2, baudrate=freq, polarity=1, phase=1, bits=8, firstbit=0, sck=Pin(18), mosi=Pin(23), miso=Pin(19))
+			print("Use hardware spi port 2: sck 18, mosi 23, miso 19")
+			self.spi = SPI(2, baudrate=freq, polarity=1, phase=1, bits=8, firstbit=0, sck=Pin(18), mosi=Pin(23), miso=Pin(19))
 		else:
+			print("Use software spi port : sck %d, mosi %d, miso %d" % (sckpin,mosipin,misopin))
 			self.spi = SPI(baudrate=freq, polarity=1, phase=1, sck=Pin(sckpin), mosi=Pin(mosipin), miso=Pin(misopin))	# software spi
 
 	def get_light_sensor_data(self):
@@ -66,7 +73,9 @@ from light_sensor import LIGHT_Sensor
 # Pin definitions
 repl_button = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
 
-light_sensor_obj = LIGHT_Sensor(freq = 1000000, port = 1, csbpin = 15)
+#light_sensor_obj = LIGHT_Sensor(freq = 1000000, port = 1, csbpin = 15)
+#light_sensor_obj = LIGHT_Sensor(freq = 1000000, port = 2, csbpin = 15)
+light_sensor_obj = LIGHT_Sensor(freq = 200000, port = 3, csbpin = 15, sckpin=0,mosipin=2,misopin=4)
 light_sensor_data = light_sensor_obj.get_light_sensor_data()
 print ("Light sensor data = %d" % light_sensor_data)
 utime.sleep_ms(100)
